566ee225d13786f562a8055ec3f5fac506e89e93
Ivan Suvalov <john@mail.emb>
2014-03-31 19:44:26 +0400

    Попробывал компилить но вышла проблема с libconfig.dll.a

--- protocol/rrw_server/build.sh
+++ protocol/rrw_server/build.sh
@@ -1,1 +1,1 @@
-gcc main.c eudp.c radar.c radar_cli.c rrw_proto.c rrw_server.c timeutils.c data_processor.c -I. -lpthread
+gcc main.c cfg.c eudp.c radar.c radar_cli.c rrw_proto.c rrw_server.c timeutils.c data_processor.c -I. -lpthread -L./lib/ -lconfig.dll
