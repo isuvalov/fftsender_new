9ba44bb38f6b58e09db45d065f14543293de479c
Ivan Suvalov <john@mail.emb>
2014-06-24 19:05:07 +0400

    Сделал matlab модель алгоритма

--- protcol_rtl/find_max.vhd
+++ protcol_rtl/find_max.vhd
@@ -25,7 +25,6 @@ architecture find_max of find_max is
 
 constant DIV_THR:std_logic_vector(15 downto 0):=conv_std_logic_vector(65536/3,16);  --# for divider 1/3
 constant AREA_SIZE:integer:=11;
-constant STEP:integer:=1;
 
 constant MUL_LATENCY:integer:=1;
 constant TRACE_LEN:integer:=AREA_SIZE+MUL_LATENCY;
@@ -39,7 +38,10 @@ signal cnt,max_pos,make_max_pos:std_logic_vector(3 downto 0):=(others=>'0');
 signal max_val,make_max_val:std_logic_vector(i_sample'Length-1 downto 0);
 signal ce_max:std_logic;
 signal mul_thr:std_logic_vector((2*i_sample'Length)-1 downto 0);
-
+signal prev_maximum_exist:std_logic:='0';
+signal can_calc:std_logic:='1';
+signal prev_maximum_val:std_logic_vector(i_sample'Length-1 downto 0):=(others=>'0');
+signal prev_maximum_exist_cnt:std_logic_vector(3 downto 0):=(others=>'1');
 
 
 begin
@@ -60,12 +62,30 @@ begin
 			--# We try to think that trace(5) is maximum
 			--# Test it by thresholds it!
 			mul_thr<=unsigned(trace(5))*unsigned(DIV_THR);
-			if not(unsigned(trace(5-STEP+MUL_LATENCY))>unsigned(mul_thr) and unsigned(trace(5+STEP+MUL_LATENCY))>unsigned(mul_thr)) then
+			if prev_maximum_exist='1' then
+				if (unsigned(trace(5))<unsigned(prev_maximum_val)) then
+					can_calc<='0';
+				else
+					can_calc<='1';
+				end if;
+			else
+				can_calc<='1';
+			end if;
+			if not(unsigned(trace(5-1+MUL_LATENCY))>unsigned(mul_thr(mul_thr'Length-1 downto mul_thr'Length-i_sample'Length)) or unsigned(trace(5+1+MUL_LATENCY))>unsigned(mul_thr)) and can_calc='1' then
 				maximum_ce<='1';
 				maximum<=trace(5+MUL_LATENCY);
 				maximum_m1<=trace(5-1+MUL_LATENCY);
 				maximum_p1<=trace(5+1+MUL_LATENCY);
+				prev_maximum_exist_cnt<=(others=>'0');
+				prev_maximum_val<=trace(5+MUL_LATENCY);
+				prev_maximum_exist<='1';
 			else
+				if unsigned(prev_maximum_exist_cnt)<AREA_SIZE/2-1 then
+					prev_maximum_exist_cnt<=prev_maximum_exist_cnt+1;
+					prev_maximum_exist<='1';
+				else
+					prev_maximum_exist<='0';
+				end if;
 				maximum_ce<='0';	
 			end if;
 		end if;
